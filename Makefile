cee3a8785c7c709779e14ee2436caa0890a0805d
kema-dev <imsofresh38760@gmail.com>
2020-11-18 00:25:10 +0100

    Beggining

---
+++ Makefile
@@ -0,0 +1,32 @@
+FLAGS = -Wall -Wextra -Werror
+
+NAME = libft.a
+
+SRC =	ft_putchar.c \
+		ft_strlen.c
+
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@ar rc $(NAME) $(OBJ)
+	@echo "$(NAME) created"
+	@ranlib $(NAME)
+	@echo "$(NAME) indexed"
+
+%.o: %.c
+	@gcc $(FLAGS) -c $< -o $@
+
+clean:
+	@rm -f $(OBJ)
+	@echo "OBJ cleaned"
+
+fclean: clean
+	@rm -f $(NAME)
+	@echo "$(NAME) cleaned"
+
+re: fclean all
+
+.PHONY: all, clean, fclean, re
---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/17 23:09:27 by jjourdan          #+#    #+#             */
+/*   Updated: 2020/11/18 00:24:04 by jjourdan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t				ft_strlen(const char *s);
+void				ft_putchar(char c);
+
+#endif
