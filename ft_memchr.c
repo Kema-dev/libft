a64fe56f6cb32edd2b8925da86ba0fd7d464c723
kema-dev <imsofresh38760@gmail.com>
2020-11-18 17:30:05 +0100

    memchr added

---
+++ ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/18 17:16:14 by jjourdan          #+#    #+#             */
+/*   Updated: 2020/11/18 17:29:31 by jjourdan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*src;
+
+	i = -1;
+	src = (char *)s;
+	while (++i < n)
+	{
+		if (src[i] == c)
+			return ((void *)(s + i));
+	}
+	return (NULL);
+}
