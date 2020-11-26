8e521d5bffd3e19be6b6688e55488e05ff6af44c
kema-dev <jeremy.jdn@free.fr>
2020-11-26 18:13:38 +0100

    next up : optimization

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,45 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 01:59:59 by jjourdan          #+#    #+#             */
-/*   Updated: 2020/11/26 17:58:09 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2020/11/26 18:13:21 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+static void	ft_filter(int n, int fd)
 {
-	char	*str;
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+		ft_putchar_fd('0', fd);
+}
 
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+void		ft_putnbr_fd(int n, int fd)
+{
+	int		i;
+	char	c[10];
+
+	if (fd < 0)
+		return ;
+	if (n == 0 || n == -2147483648)
+	{
+		ft_filter(n, fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	i = 0;
+	while (n > 0)
+	{
+		c[i] = n % 10 + '0';
+		n = n / 10;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+		ft_putchar_fd(c[i--], fd);
 }
