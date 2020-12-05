aee0618db6656517ddef1c11b638c91efd23e6fe
kema-dev <imsofresh38760@gmail.com>
2020-12-05 16:15:54 +0100

    corrr

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 02:01:02 by jjourdan          #+#    #+#             */
-/*   Updated: 2020/12/05 16:15:18 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2020/12/05 16:15:50 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 	int	size_src;
-	int		size_src;
 
 	i = 0;
 	size_src = 0;
