5543e8111cc9c9e6e53961f1f011a33e78847d3d
kema-dev <imsofresh38760@gmail.com>
2020-12-05 16:21:02 +0100

    final strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 02:01:02 by jjourdan          #+#    #+#             */
-/*   Updated: 2020/12/05 16:19:35 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2020/12/05 16:20:57 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	if (!dest || !src)
 		return (0);
 	i = 0;
-	size_src = 0;
-	while (src[size_src] != 0)
-		size_src++;
+	size_src = ft_strlen(src);
 	while (((int)i < (int)size - 1) && (src[i] != 0))
 	{
 		dest[i] = src[i];
