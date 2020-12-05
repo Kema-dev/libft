af51894c5f39c8c41726e5dc1d40d112509c4af0
kema-dev <imsofresh38760@gmail.com>
2020-12-05 16:12:14 +0100

    strlcat corr again and again

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 02:01:02 by jjourdan          #+#    #+#             */
-/*   Updated: 2020/12/05 16:11:20 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2020/12/05 16:12:06 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 
 	//if (!src)
 	//	return (0);
-	if (!dest)
-		return (size_src);
 	i = 0;
 	size_src = 0;
 	size_src += ft_strlen(src);
+	if (!dest)
+		return (size_src);
 	while (((int)i < (int)size - 1) && (src[i] != 0))
 	{
 		dest[i] = src[i];
