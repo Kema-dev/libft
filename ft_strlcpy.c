e43677baad2377464032e4a72e12f70f0c403bcd
kema-dev <imsofresh38760@gmail.com>
2020-12-05 16:15:23 +0100

    corr ?

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 02:01:02 by jjourdan          #+#    #+#             */
-/*   Updated: 2020/12/05 16:12:06 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2020/12/05 16:15:18 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
+	int	size_src;
 	int		size_src;
 
-	//if (!src)
-	//	return (0);
 	i = 0;
 	size_src = 0;
-	size_src += ft_strlen(src);
-	if (!dest)
-		return (size_src);
+	while (src[size_src] != 0)
+		size_src++;
 	while (((int)i < (int)size - 1) && (src[i] != 0))
 	{
 		dest[i] = src[i];
