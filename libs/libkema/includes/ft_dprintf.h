dbff0ca7c585aa22e7755c346a202b5a700e83bf
Kema-dev <jeremy.jdn@free.fr>
2021-04-22 14:55:00 -0400

    added colors

--- libs/libkema/includes/ft_dprintf.h
+++ libs/libkema/includes/ft_dprintf.h
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 14:19:20 by jjourdan          #+#    #+#             */
-/*   Updated: 2021/04/22 14:00:35 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2021/04/22 14:53:07 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,59 @@ typedef struct s_flag
 	char	type;
 	size_t	tot_len;
 }	t_flag;
+
+//	*	formats
+
+# define DEFAULT			"\e[0m"
+# define BOLD				"\e[1m"
+# define DIM				"\e[2m"
+# define ITALIC				"\e[3m"
+# define UNDERLINED			"\e[4m"
+# define BLINK				"\e[5m"
+# define REVERSE			"\e[7m"
+# define HIDDEN				"\e[8m"
+
+# define NO_FORMAT			"\e[22m"
+
+//	*	text colors
+
+# define NO_COLOR			"\e[39m"
+# define BLACK				"\e[30m"
+# define RED				"\e[31m"
+# define GREEN				"\e[32m"
+# define YELLOW				"\e[33m"
+# define BLUE				"\e[34m"
+# define MAGENTA			"\e[35m"
+# define CYAN				"\e[36m"
+# define LIGHT_GRAY			"\e[37m"
+# define DARK_GRAY			"\e[90m"
+# define LIGHT_RED			"\e[91m"
+# define LIGHT_GREEN		"\e[92m"
+# define LIGHT_YELLOW		"\e[93m"
+# define LIGHT_BLUE			"\e[94m"
+# define LIGHT_MAGENTA		"\e[95m"
+# define LIGHT_CYAN			"\e[96m"
+# define WHITE				"\e[97m"
+
+//	*	background colors
+
+# define BACK_NO_COLOR		"\e[49m"
+# define BACK_BLACK			"\e[40m"
+# define BACK_RED			"\e[41m"
+# define BACK_GREEN			"\e[42m"
+# define BACK_YELLOW		"\e[43m"
+# define BACK_BLUE			"\e[44m"
+# define BACK_MAGENTA		"\e[45m"
+# define BACK_CYAN			"\e[46m"
+# define BACK_LIGHT_GRAY	"\e[47m"
+# define BACK_DARK_GRAY		"\e[100m"
+# define BACK_LIGHT_RED		"\e[101m"
+# define BACK_LIGHT_GREEN	"\e[102m"
+# define BACK_LIGHT_YELLOW	"\e[103m"
+# define BACK_LIGHT_BLUE	"\e[104m"
+# define BACK_LIGHT_MAGENTA	"\e[105m"
+# define BACK_LIGHT_CYAN	"\e[106m"
+# define BACK_WHITE			"\e[107m"
 
 int					ft_dprintf_atoi(const char *nptr);
 char				*ft_dprintf_itoa(int n);
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 10:38:50 by jjourdan          #+#    #+#             */
-/*   Updated: 2021/04/22 14:16:41 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2021/04/22 14:54:53 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	str = kemalloc(14, sizeof(char));
-	ft_dprintf(STDOUT_FILENO, "Enter a string : ");
+	str = kemalloc(18, sizeof(char));
+	ft_strlcpy(str, "Enter a string : ", 18);
+	ft_dprintf(STDOUT_FILENO, LIGHT_BLUE BOLD "%s" DEFAULT DIM, str);
+	kemafree(str);
 	get_next_line(STDIN_FILENO, &str);
-	ft_dprintf(STDOUT_FILENO, "You entered \"%s\" which is %d chars long\n", str, ft_strlen(str));
+	ft_dprintf(STDOUT_FILENO, DEFAULT "You entered \"%s\" which is %d chars long\n", str, ft_strlen(str));
 	exit(kemaexit(SUCCESS));
 }
